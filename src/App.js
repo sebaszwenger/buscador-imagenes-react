03df83ee8783cf3b557757797e295840e02e1449
sebaszwenger <sebaszwenger@gmail.com>
2021-10-12 20:42:49 -0300

    new usestate "search" to retrieve the search term. new usestate "images" to save the result of the call to the api. implementation of component ListadoImagenes.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import Formulario from "./components/Formulario";
+import ListadoFotos from "./components/ListadoImagenes";
+import { useState, useEffect } from "react";
 
 function App() {
+  //State de la app
+  const [busqueda, setBusqueda] = useState("");
+  const [imagenes, setImagenes] = useState([]);
+
+  useEffect(() => {
+    if (busqueda) {
+      const consultarAPI = async () => {
+        const apiKey = "234385-177973b90356ea885b6d5323b";
+        const imagenesPorPagina = 30;
+        const url = `https://pixabay.com/api/?key=${apiKey}&q=${busqueda}&per_page=${imagenesPorPagina}`;
+
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        setImagenes(resultado.hits);
+
+        // setResultado(resultado);
+        // setConsultar(false);
+
+        //Detecta si hubo resultados correctos con la consulta
+        // if (resultado.cod === "404") {
+        //   setError(true);
+        // } else {
+        //   setError(false);
+        // }
+      };
+      consultarAPI();
+    }
+  }, [busqueda]);
+
   return (
     <div className="container">
       <div className="jumbotron">
         <p className="lead text-center">Buscador de Imágenes</p>
 
-        <Formulario />
+        <Formulario setBusqueda={setBusqueda} />
+      </div>
+
+      <div className="row justify-content-center">
+        <ListadoFotos imagenes={imagenes} />
       </div>
     </div>
   );
