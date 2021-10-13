850fb2d25b453e3cb600e217d99b1e39110639dd
sebaszwenger <sebaszwenger@gmail.com>
2021-10-13 19:34:51 -0300

    Implementation and conditional loading of pagination buttons. Moved the screen to the top on clicking the pagination buttons. translated all spanish words to english. Documentation.

--- src/App.js
+++ src/App.js
@@ -1,42 +1,84 @@
-import Formulario from "./components/Formulario";
-import ListadoFotos from "./components/ListadoImagenes";
+import Form from "./components/Form";
+import Listimages from "./components/Listimages";
 import { useState, useEffect } from "react";
 
 function App() {
-  //State de la app
-  const [busqueda, setBusqueda] = useState("");
-  const [imagenes, setImagenes] = useState([]);
-  const [paginaActual, setPaginaActual] = useState(1);
-  const [totalPaginas, setTotalPaginas] = useState(1);
+  //States
+  const [search, setSearch] = useState("");
+  const [images, setImages] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pages, setPages] = useState(1);
 
+  //Refresh the call to the API on change of search & pagina Actual
   useEffect(() => {
-    if (busqueda) {
-      const consultarAPI = async () => {
+    //avoid first call with empty search term
+    if (search) {
+      const queryAPI = async () => {
         const apiKey = "234385-177973b90356ea885b6d5323b";
-        const imagenesPorPagina = 30;
-        const url = `https://pixabay.com/api/?key=${apiKey}&q=${busqueda}&per_page=${imagenesPorPagina}`;
+        const imagesPerPage = 20;
+        const url = `https://pixabay.com/api/?key=${apiKey}&q=${search}&per_page=${imagesPerPage}&page=${currentPage}`;
 
-        const respuesta = await fetch(url);
-        const resultado = await respuesta.json();
-        const totalPaginas = Math.ceil(resultado.totalHits / imagenesPorPagina);
+        const answer = await fetch(url);
+        const result = await answer.json();
 
-        setTotalPaginas(totalPaginas);
-        setImagenes(resultado.hits);
+        const totalPages = Math.ceil(result.totalHits / imagesPerPage);
+
+        setPages(totalPages);
+        setImages(result.hits);
+
+        //Move the screen to top smoothly
+        const jumbotron = document.querySelector(".jumbotron");
+        jumbotron.scrollIntoView({ behavior: "smooth" });
       };
-      consultarAPI();
+      queryAPI();
     }
-  }, [busqueda]);
+  }, [search, currentPage]);
+
+  //define the previous page
+  const previousPage = () => {
+    const newCurrentPage = currentPage - 1;
+    if (newCurrentPage === 0) return;
+    setCurrentPage(newCurrentPage);
+  };
+  //define the next page
+  const nextPage = () => {
+    const newCurrentPage = currentPage + 1;
+    if (newCurrentPage > pages) return;
+    setCurrentPage(newCurrentPage);
+  };
+
+  //Conditional loading of paging buttons
+  const previusButton =
+    currentPage === 1 ? null : (
+      <button
+        type="button"
+        className="bbtn btn-info mr-1 mb-2"
+        onClick={previousPage}
+      >
+        &laquo; Anterior
+      </button>
+    );
+  const nextButton =
+    currentPage === pages ? null : (
+      <button type="button" className="bbtn btn-info mb-2" onClick={nextPage}>
+        Siguiente &raquo;
+      </button>
+    );
 
   return (
     <div className="container">
       <div className="jumbotron">
         <p className="lead text-center">Buscador de Imágenes</p>
 
-        <Formulario setBusqueda={setBusqueda} />
+        <Form setSearch={setSearch} />
       </div>
 
       <div className="row justify-content-center">
-        <ListadoFotos imagenes={imagenes} />
+        <Listimages images={images} />
+
+        {previusButton}
+
+        {nextButton}
       </div>
     </div>
   );
