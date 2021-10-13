c8f1b92e88ddaaedabbd9b608c91b945d1873970
sebaszwenger <sebaszwenger@gmail.com>
2021-10-13 19:16:08 -0300

    add proptypes

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,9 +1,14 @@
-const Error = ({ mensaje }) => {
+import PropTypes from "prop-types";
+
+const Error = ({ message }) => {
   return (
     <>
-      <p className="my-3 p-3 text-center alert-primary alert">{mensaje}</p>
+      <p className="my-3 p-3 text-center alert-primary alert">{message}</p>
     </>
   );
 };
 
+Error.propTypes = {
+  message: PropTypes.string.isRequired,
+};
 export default Error;
